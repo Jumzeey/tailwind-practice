8776dd1c4bc6d74bd5d78cd298b22f6f092155d5
jumzeey <umatonjayrex01@gmail.com>
2023-03-25 17:19:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      lineHeight: {
+        '15': '1.2'
+      },
+      colors:{
+        'turquiose': '#358f80',
+        'button': '#248277'
+      }
+    },
+  },
+  plugins: [],
+}
